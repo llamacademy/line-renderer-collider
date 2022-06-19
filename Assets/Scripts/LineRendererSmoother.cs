8ba41ac645558eeb489e79b1f55a8d064a4ada31
Chris Kurhan <support@llama.software>
2022-06-19 12:33:03 -0500

    Initial Commit

---
+++ Assets/Scripts/LineRendererSmoother.cs
@@ -0,0 +1,91 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineRendererSmoother : MonoBehaviour
+{
+    public LineRenderer Line;
+    public Vector3[] InitialState = new Vector3[1];
+    public float SmoothingLength = 2f;
+    public int SmoothingSections = 10;
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 300, 40), "Smooth Line"))
+        {
+            Smooth();
+        }
+        if (GUI.Button(new Rect(10, 60, 300, 40), "Generate Collider"))
+        {
+            GenerateMeshCollider();
+        }
+        if (GUI.Button(new Rect(10, 110, 300, 40), "Simplify Mesh"))
+        {
+            Line.Simplify(0.1f);
+        }
+    }
+
+    public void GenerateMeshCollider()
+    {
+        MeshCollider collider = GetComponent<MeshCollider>();
+
+        if (collider == null)
+        {
+            collider = gameObject.AddComponent<MeshCollider>();
+        }
+
+
+        Mesh mesh = new Mesh();
+        Line.BakeMesh(mesh, true);
+        collider.sharedMesh = mesh;
+    }
+
+    public void Smooth()
+    {
+        BezierCurve[] curves = new BezierCurve[Line.positionCount - 1];
+        for (int i = 0; i < curves.Length; i++)
+        {
+            curves[i] = new BezierCurve();
+        }
+
+        for (int i = 0; i < curves.Length; i++)
+        {
+            Vector3 position = Line.GetPosition(i);
+            Vector3 lastPosition = i == 0 ? Line.GetPosition(0) : Line.GetPosition(i - 1);
+            Vector3 nextPosition = Line.GetPosition(i + 1);
+
+            Vector3 lastDirection = (position - lastPosition).normalized;
+            Vector3 nextDirection = (nextPosition - position).normalized;
+
+            Vector3 startTangent = (lastDirection + nextDirection) * SmoothingLength;
+            Vector3 endTangent = (nextDirection + lastDirection) * -1 * SmoothingLength;
+
+
+            curves[i].Points[0] = position; // Start Position (P0)
+            curves[i].Points[1] = position + startTangent; // Start Tangent (P1)
+            curves[i].Points[2] = nextPosition + endTangent; // End Tangent (P2)
+            curves[i].Points[3] = nextPosition; // End Position (P3)
+        }
+
+        // Apply look-ahead for first curve and retroactively apply the end tangent
+        {
+            Vector3 nextDirection = (curves[1].EndPosition - curves[1].StartPosition).normalized;
+            Vector3 lastDirection = (curves[0].EndPosition - curves[0].StartPosition).normalized;
+
+            curves[0].Points[2] = curves[0].Points[3] +
+                (nextDirection + lastDirection) * -1 * SmoothingLength;
+        }
+
+        Line.positionCount = curves.Length * SmoothingSections;
+        int index = 0;
+        for (int i = 0; i < curves.Length; i++)
+        {
+            Vector3[] segments = curves[i].GetSegments(SmoothingSections);
+            for (int j = 0; j < segments.Length; j++)
+            {
+                Line.SetPosition(index, segments[j]);
+                index++;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# How to Generate Colliders for Line Renderers
+Learn how to dynamically generate a collider that follows your Line Renderer regardless of shape or size of your Line Renderer! In this tutorial I cover how to add a new Mesh Collider component to your Line Renderer GameObject and generate a mesh at runtime that matches your Line Renderer!
+
+It's easier than you might think!
+
+[![Youtube Tutorial](./Video%20Screenshot.png)](https://youtu.be/2BH1yQXCpeU)
+
+## Patreon Supporters
+Have you been getting value out of these tutorials? Do you believe in LlamAcademy's mission of helping everyone make their game dev dream become a reality? Consider becoming a Patreon supporter and get your name added to this list, as well as other cool perks.
+Head over to https://patreon.com/llamacademy to show your support.
+
+### Phenomenal Supporter Tier
+* YOUR NAME HERE!
+
+### Tremendous Supporter Tier
+* YOUR NAME HERE!
+
+### Awesome Supporter Tier
+* Raphael
+* Andrew Bowen
+* Gerald Anderson
+* AudemKay
+* Paul Berry
+* Matt Parkin
+* Ivan
+* YOUR NAME HERE!
+
+### Supporters
+* Bastian
+* Trey Briggs
+* YOUR NAME HERE!
+
+## Other Projects
+Interested in other AI Topics in Unity, or other tutorials on Unity in general? 
+
+* [Check out the LlamAcademy YouTube Channel](https://youtube.com/c/LlamAcademy)!
+* [Check out the LlamAcademy GitHub for more projects](https://github.com/llamacademy)
+
+## Requirements
+* Requires Unity 2021.3 LTS or higher. (technically the API is supported from 2018, but I haven't tried older versions of Unity with this project)
